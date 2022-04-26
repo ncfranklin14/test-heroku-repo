180e8111204a2a8d7d676df5a1d822fb43bf45a4
Nicole Franklin <Ncfranklin14@gmail.com>
2022-04-26 14:26:45 -0700

    heroku config

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+const path = require("path");
+
+const people = [ 
+    {
+        id:1,
+        name:"Joe",
+        role:"Instructor"
+    },
+    {
+        id:2,
+        name:"Niles",
+        role:"TA"
+    },
+          {
+        id:3,
+        name:"Stefan",
+        role:"Student"
+    }
+]
+// parsing data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// serves css/js/static assets
+app.use(express.static("public"))
+const allRoutes = require("./routes")
+app.use(allRoutes);
+
+app.listen(PORT,()=>{
+    console.log("listening to port " + PORT)
+})
